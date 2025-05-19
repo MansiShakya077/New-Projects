d9939bcb8df026ed28b4e63d9f1ad21fd341e00f
MansiShakya077 <mansishakya21@gmail.com>
2025-05-19 10:31:11 +0530

    Add files via upload

---
+++ image search engin/script.js
@@ -0,0 +1,6 @@
+const searchform=document.getElementById("search-form");
+const searchbox=document.getElementById("search-box");
+const searchResult=document.getElementById("search-result");
+const showMoreBtn=document.getElementById("show-more-btn");
+
+
